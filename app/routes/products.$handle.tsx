ae4a4f0142f54339f495d377aacc2f4b847efcda
McFly <matthieu.roman2002@gmail.com>
2024-04-21 19:14:53 +0200

    push29

--- app/routes/products.$handle.tsx
+++ app/routes/products.$handle.tsx
@@ -254,6 +254,23 @@ export default function Product() {
         setTobasket(false)
     }
 
+    const [scrollPosition, setScrollPosition] = useState(0)
+
+    useEffect(() => {
+        function handleScroll() {
+            const position = window.scrollY
+            setScrollPosition(position)
+        }
+
+        // Ajoute un écouteur d'événement pour le défilement lors du montage du composant
+        window.addEventListener('scroll', handleScroll)
+
+        // Nettoie l'écouteur d'événement lors du démontage du composant
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, []) // le tableau vide indique que cet effet ne dépend d'aucune valeur, il ne se déclenchera donc qu'une fois au montage du composant
+
     return (
         <>
             {crownsOpen && <Crowns isOpen={openCronws} onClose={closeCrowns} />}
@@ -261,10 +278,10 @@ export default function Product() {
             {size && <Size isOpen={openSize} onClose={closeSize} />}
             <div
                 style={{
-                    marginTop: width > 768 ? '300px' : '200px',
+                    marginTop: width > 768 ? '300px' : '170px',
                 }}
             >
-                {width < 768 && (
+                {width < 768 && scrollPosition < 1000 && (
                     <div
                         className={`product-menu ${expanded ? 'expanded' : ''}`}
                         style={{
@@ -468,6 +485,7 @@ export default function Product() {
                                             style={{
                                                 marginRight: '10px',
                                                 opacity: '1 !important',
+                                                width: '25px',
                                             }}
                                         />
                                         Random item
@@ -478,43 +496,183 @@ export default function Product() {
                     </div>
                 </>
             ) : (
-                <div className='product-desc'>
-                    <div className='product-caracteristics'>
-                        {product?.daterelease && (
-                            <p>
-                                Date de sortie : {product?.daterelease?.value}
-                            </p>
-                        )}
-
-                        {product?.colors && (
-                            <p>
-                                couleurs :
-                                {parsedColors?.map((color: any) => color + ' ')}
-                            </p>
-                        )}
-
-                        {product?.materials && (
-                            <p>{product?.materials?.value}</p>
-                        )}
-                    </div>
-                    <div className='product-description'>
-                        {!showText1 ? (
-                            <div>{product.description.slice(0, 80)}...</div>
-                        ) : (
-                            <div
-                                dangerouslySetInnerHTML={{
-                                    __html: product.descriptionHtml,
+                <>
+                    <div className='product-desc'>
+                        <div className='product-caracteristics'>
+                            <h4
+                                style={{
+                                    marginBottom: '20px',
                                 }}
-                            />
-                        )}
-                        <button
-                            className='responsive-btn-show'
-                            onClick={() => setShowText1(!showText1)}
-                        >
-                            {showText1 ? '-' : '+'}
-                        </button>
+                            >
+                                Caractéristiques
+                            </h4>
+                            {product?.daterelease && (
+                                <p>
+                                    Date de sortie :{' '}
+                                    {product?.daterelease?.value}
+                                </p>
+                            )}
+
+                            {product?.colors && (
+                                <p>
+                                    couleurs :
+                                    {parsedColors?.map(
+                                        (color: any) => color + ' '
+                                    )}
+                                </p>
+                            )}
+
+                            {product?.materials && (
+                                <p>{product?.materials?.value}</p>
+                            )}
+                        </div>
+                        <div className='product-description'>
+                            <h4
+                                style={{
+                                    marginBottom: '20px',
+                                }}
+                            >
+                                Description
+                            </h4>
+                            {!showText1 ? (
+                                <div>{product.description.slice(0, 80)}...</div>
+                            ) : (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: product.descriptionHtml,
+                                    }}
+                                />
+                            )}
+                            <button
+                                className='responsive-btn-show'
+                                onClick={() => setShowText1(!showText1)}
+                            >
+                                {showText1 ? '-' : '+'}
+                            </button>
+                        </div>
                     </div>
-                </div>
+
+                    <>
+                        <TrackT
+                            products={productsFromCollection}
+                            title='Produits connexes'
+                        />
+                        <div className='productBanner'>
+                            <video
+                                width='100%'
+                                height='auto'
+                                autoPlay
+                                muted
+                                loop
+                            >
+                                <source
+                                    src='/product/banner.mp4'
+                                    type='video/mp4'
+                                />
+                                <img src='/product/banner.png' alt='banner' />
+                            </video>
+                        </div>
+                        <TrackT
+                            products={random}
+                            title={randomNameFunction()}
+                        />
+                        <MarketDrag />
+
+                        <div
+                            className='gofilters'
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection: 'column',
+                                marginBottom: '200px',
+                                textAlign: 'center',
+                            }}
+                        >
+                            <h1
+                                style={{
+                                    marginBottom: '10px',
+                                }}
+                            >
+                                Pas encore trouvé votre perle rare ?
+                            </h1>
+                            <p>
+                                Nous avons une multitude de styles et de pièces
+                                uniques, mais nous savons que parfois, vous
+                                cherchez quelque chose de très spécifique.{' '}
+                                <br />
+                                Si vous n’avez pas encore trouvé exactement ce
+                                que vous voulez dans notre Panel Trackt, nous
+                                sommes là pour vous aider.
+                            </p>
+                            <div className='gofilters-btn'>
+                                <div className='four-btns'>
+                                    <Link to='/filters'>
+                                        <button>
+                                            <img
+                                                src='/filters/checkbox.png'
+                                                alt='checkbox'
+                                                style={{
+                                                    width: '20px',
+                                                    height: '20px',
+                                                    marginRight: '10px',
+                                                    opacity: '1 !important',
+                                                }}
+                                            />
+                                            Rechercher par filtres
+                                        </button>
+                                    </Link>
+                                    <a href='#categories-aside'>
+                                        <button>Shopping par catégories</button>
+                                    </a>
+                                    <a href='#search-aside'>
+                                        <button>
+                                            <svg
+                                                id='icon'
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                width='21.548'
+                                                height='21.547'
+                                                viewBox='0 0 21.548 21.547'
+                                                style={{
+                                                    marginRight: '10px',
+                                                }}
+                                            >
+                                                <path
+                                                    id='Tracé_219'
+                                                    data-name='Tracé 219'
+                                                    d='M988.192,241.428a8.08,8.08,0,1,1,8.08-8.08A8.089,8.089,0,0,1,988.192,241.428Zm0-13.467a5.387,5.387,0,1,0,5.387,5.387A5.393,5.393,0,0,0,988.192,227.961Z'
+                                                    transform='translate(-980.112 -225.268)'
+                                                    fill='#fff'
+                                                />
+                                                <path
+                                                    id='Tracé_220'
+                                                    data-name='Tracé 220'
+                                                    d='M997.192,243.695a1.337,1.337,0,0,1-.952-.395l-6.734-6.733a1.346,1.346,0,0,1,1.9-1.9l6.734,6.733a1.347,1.347,0,0,1-.952,2.3Z'
+                                                    transform='translate(-976.992 -222.148)'
+                                                    fill='#fff'
+                                                />
+                                            </svg>
+                                            Rechercher manuellement
+                                        </button>
+                                    </a>
+                                    <Link to='/filtered'>
+                                        <button>
+                                            <img
+                                                src='/filters/arrow-shuffle.png'
+                                                alt='arrow-shuffle'
+                                                style={{
+                                                    marginRight: '10px',
+                                                    opacity: '1 !important',
+                                                }}
+                                            />
+                                            Random item
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                </>
             )}
             {width < 768 && (
                 <>
@@ -571,11 +729,25 @@ export default function Product() {
                             </Suspense>
                         </div>
                     )}
-                    {!tobasket && (
+                    {!tobasket && scrollPosition < 1000 && (
                         <div className='choose-size'>
                             <h6 onClick={() => setTobasket(!tobasket)}>
                                 Choisir ma taille
                             </h6>
+                        </div>
+                    )}
+                    {scrollPosition > 1000 && (
+                        <div className='product-header-sticky'>
+                            <div className='product-header-sticky-product'>
+                                <img
+                                    src={product?.selectedVariant?.image?.url}
+                                    alt={product?.vendor}
+                                />
+                                <div className='product-header-sticky-product-info'>
+                                    <p>{product.vendor}</p>
+                                    <p>{product.title}</p>
+                                </div>
+                            </div>
                         </div>
                     )}
                 </>
@@ -748,11 +920,28 @@ function BreadCrumb({ product }: { product: any }) {
                         {isMixte}
                     </Link>{' '}
                     &gt; <Link to={'/search?q=' + type}>{type}</Link> &gt;{' '}
-                    <Link to={'/search?q=' + vendor}>{vendor}</Link> &gt;{' '}
-                    <Link to={'/search?q=' + collection}>{collection}</Link>{' '}
+                    <Link to={'/search?q=' + vendor}>
+                        {width > 768
+                            ? vendor
+                            : vendor?.length > 15
+                            ? vendor.slice(0, 15) + '...'
+                            : vendor}
+                    </Link>{' '}
+                    &gt;{' '}
+                    <Link to={'/search?q=' + collection}>
+                        {width > 768
+                            ? collection
+                            : collection?.length > 15
+                            ? collection.slice(0, 15) + '...'
+                            : collection}
+                    </Link>{' '}
                     &gt;{' '}
                     <Link to={'/products/' + product.handle}>
-                        {productName}
+                        {width > 768
+                            ? productName
+                            : productName?.length > 10
+                            ? productName.slice(0, 10) + '...'
+                            : productName}
                     </Link>
                 </p>
                 <div className='breadcrumb-colors'>
@@ -838,7 +1027,13 @@ function ProductImage({ image, product }: { image: any; product: any }) {
             <div className='product-image'>
                 <div className='product-image-title'>
                     <Link to={'/search?q=' + product.vendor}>
-                        <h1>{product.vendor}</h1>
+                        <h1
+                            style={{
+                                marginBottom: width > 768 ? '' : '5px',
+                            }}
+                        >
+                            {product.vendor}
+                        </h1>
                     </Link>
                     <Link
                         to={'/search?q=' + product.collections?.nodes[0].title}
