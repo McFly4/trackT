2a30786b68cd10e8c0f14ed8b33b3fc6e6675a7b
McFly <matthieu.roman2002@gmail.com>
2024-07-17 18:13:48 +0200

    remove thing

--- app/components/AsideCategories.tsx
+++ app/components/AsideCategories.tsx
@@ -419,83 +419,83 @@ export function AsideCategories({
               </div>
             </div>
           </div>
-          <div className='categoriesAsideNav'>
-            <h5>vous pouvez également essayer ces recherches</h5>
-            <div
-              className='four-btns'
-              style={{
-                marginTop: '40px',
-              }}
-            >
-              {/*<button*/}
-              {/*  onClick={() => {*/}
-              {/*    window.location.href = '/filters'*/}
-              {/*  }}*/}
-              {/*>*/}
-              {/*  <img*/}
-              {/*    src='/filters/checkbox.png'*/}
-              {/*    alt='check'*/}
-              {/*    style={{*/}
-              {/*      width: '20px',*/}
-              {/*      marginRight: '10px',*/}
-              {/*    }}*/}
-              {/*  />*/}
-              {/*  Rechercher par filtres*/}
-              {/*</button>*/}
+          {/*<div className='categoriesAsideNav'>*/}
+          {/*  <h5>vous pouvez également essayer ces recherches</h5>*/}
+          {/*  <div*/}
+          {/*    className='four-btns'*/}
+          {/*    style={{*/}
+          {/*      marginTop: '40px',*/}
+          {/*    }}*/}
+          {/*  >*/}
+          {/*    /!*<button*!/*/}
+          {/*    /!*  onClick={() => {*!/*/}
+          {/*    /!*    window.location.href = '/filters'*!/*/}
+          {/*    /!*  }}*!/*/}
+          {/*    /!*>*!/*/}
+          {/*    /!*  <img*!/*/}
+          {/*    /!*    src='/filters/checkbox.png'*!/*/}
+          {/*    /!*    alt='check'*!/*/}
+          {/*    /!*    style={{*!/*/}
+          {/*    /!*      width: '20px',*!/*/}
+          {/*    /!*      marginRight: '10px',*!/*/}
+          {/*    /!*    }}*!/*/}
+          {/*    /!*  />*!/*/}
+          {/*    /!*  Rechercher par filtres*!/*/}
+          {/*    /!*</button>*!/*/}
 
-              <button
-                onClick={() => {
-                  history.go(-1)
-                  window.location.href = '#searcha-side'
-                }}
-              >
-                <svg
-                  id='icon'
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='21.548'
-                  height='21.547'
-                  viewBox='0 0 21.548 21.547'
-                  style={{
-                    marginRight: '10px',
-                  }}
-                >
-                  <path
-                    id='Tracé_219'
-                    data-name='Tracé 219'
-                    d='M988.192,241.428a8.08,8.08,0,1,1,8.08-8.08A8.089,8.089,0,0,1,988.192,241.428Zm0-13.467a5.387,5.387,0,1,0,5.387,5.387A5.393,5.393,0,0,0,988.192,227.961Z'
-                    transform='translate(-980.112 -225.268)'
-                    fill='#fff'
-                  />
-                  <path
-                    id='Tracé_220'
-                    data-name='Tracé 220'
-                    d='M997.192,243.695a1.337,1.337,0,0,1-.952-.395l-6.734-6.733a1.346,1.346,0,0,1,1.9-1.9l6.734,6.733a1.347,1.347,0,0,1-.952,2.3Z'
-                    transform='translate(-976.992 -222.148)'
-                    fill='#fff'
-                  />
-                </svg>
-                Rechercher manuellement
-              </button>
-              <button
-                onClick={() => {
-                  window.location.href = '/filtered'
-                }}
-                style={{
-                  backgroundImage: 'url(/random.png)',
-                }}
-              >
-                <img
-                  src='/filters/arrow-shuffle.png'
-                  alt='arrow'
-                  style={{
-                    marginRight: '10px',
-                    width: '20px',
-                  }}
-                />
-                Random item
-              </button>
-            </div>
-          </div>
+          {/*    <button*/}
+          {/*      onClick={() => {*/}
+          {/*        history.go(-1)*/}
+          {/*        window.location.href = '#searcha-side'*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <svg*/}
+          {/*        id='icon'*/}
+          {/*        xmlns='http://www.w3.org/2000/svg'*/}
+          {/*        width='21.548'*/}
+          {/*        height='21.547'*/}
+          {/*        viewBox='0 0 21.548 21.547'*/}
+          {/*        style={{*/}
+          {/*          marginRight: '10px',*/}
+          {/*        }}*/}
+          {/*      >*/}
+          {/*        <path*/}
+          {/*          id='Tracé_219'*/}
+          {/*          data-name='Tracé 219'*/}
+          {/*          d='M988.192,241.428a8.08,8.08,0,1,1,8.08-8.08A8.089,8.089,0,0,1,988.192,241.428Zm0-13.467a5.387,5.387,0,1,0,5.387,5.387A5.393,5.393,0,0,0,988.192,227.961Z'*/}
+          {/*          transform='translate(-980.112 -225.268)'*/}
+          {/*          fill='#fff'*/}
+          {/*        />*/}
+          {/*        <path*/}
+          {/*          id='Tracé_220'*/}
+          {/*          data-name='Tracé 220'*/}
+          {/*          d='M997.192,243.695a1.337,1.337,0,0,1-.952-.395l-6.734-6.733a1.346,1.346,0,0,1,1.9-1.9l6.734,6.733a1.347,1.347,0,0,1-.952,2.3Z'*/}
+          {/*          transform='translate(-976.992 -222.148)'*/}
+          {/*          fill='#fff'*/}
+          {/*        />*/}
+          {/*      </svg>*/}
+          {/*      Rechercher manuellement*/}
+          {/*    </button>*/}
+          {/*    <button*/}
+          {/*      onClick={() => {*/}
+          {/*        window.location.href = '/filtered'*/}
+          {/*      }}*/}
+          {/*      style={{*/}
+          {/*        backgroundImage: 'url(/random.png)',*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <img*/}
+          {/*        src='/filters/arrow-shuffle.png'*/}
+          {/*        alt='arrow'*/}
+          {/*        style={{*/}
+          {/*          marginRight: '10px',*/}
+          {/*          width: '20px',*/}
+          {/*        }}*/}
+          {/*      />*/}
+          {/*      Random item*/}
+          {/*    </button>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </main>
       </div>
     </div>
